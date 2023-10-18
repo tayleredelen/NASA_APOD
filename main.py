805d4dfc1cfd3d75335d321d9e2255a41f55abf5
tayleredelen <taylerledelen@gmail.com>
2023-10-18 11:16:05 -0500

    Solved

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import streamlit as st
 import requests
 
+# prepare API key and API url
 api_key = ("HzORnbY4yEtkpGLiS9g2edxEODi5pgqLgvA4dDtF")
-url = ("https://api.nasa.gov/planetary/apod"
-       f"{api_key}")
+url = ("https://api.nasa.gov/planetary/apod?"
+       f"api_key={api_key}")
 
-request = requests.get(url)
+# get the request data as a dictionary
+data_response = requests.get(url)
+data = data_response.json()
 
-content = request.json()
+# extract the image title, url, and explanation
+title = data["title"]
+image_url = data["url"]
+explanation = data["explanation"]
 
-body = ("")
-for picture in content:
-    body = picture["image"]
-    print(body)
+# download the image
+image_filepath = "img.png"
+image_response = requests.get(image_url)
+with open(image_filepath, "wb") as file:
+    file.write(image_response.content)
+
+st.title(title)
+st.image(image_filepath)
+st.write(explanation)
